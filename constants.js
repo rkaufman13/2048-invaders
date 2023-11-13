8ab0d3b52d12da0db896bfeb7e4752858188101c
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2023-11-13 16:04:54 -0500

    volume control

--- constants.js
+++ constants.js
@@ -22,3 +22,10 @@ export const TOP_BUFFER = 50;
 export const LEFT_BUFFER = 25;
 export const RIGHT_BUFFER = 15;
 export const FINISHED_SPRITES_ARRAY = ["2", "4", "8", "16"];
+export const basicFontConfig = {
+  fontSize: "15px",
+  fill: "#fff",
+};
+
+export const instructions =
+  "You are a brave fighter who hates math. Your enemies are growing exponentionally larger by the minute. Your only hope to defeat them is to combine them into the largest known number: 2048.\nShoot the number enemies on the screen, trying to match two of the same number. If you do that, they'll combine into one bigger enemy. Keep doing this until 2048 appears, and then kill it!";
--- settings.js
+++ settings.js
@@ -1,3 +1,5 @@
+import { basicFontConfig, instructions } from "./constants.js";
+
 export default class Settings extends Phaser.Scene {
   constructor() {
     super("settings");
@@ -8,23 +10,53 @@ export default class Settings extends Phaser.Scene {
     this.load.image("leftclick", "assets/settings/left_click.png");
     this.load.image("rightclick", "assets/settings/right_click.png");
     this.load.image("handle", "assets/settings/handle.png");
+    this.load.image("close", "assets/settings/close.png");
+    this.load.image("bg-box", "assets/settings/settings_box.png");
+    this.load.image("heal", "assets/health-powerup.png");
+    this.load.spritesheet("magnet", "assets/mega-powerup.png", {
+      frameWidth: 142,
+      frameHeight: 142,
+    });
   }
 
   create() {
-    let volume = 100;
-    this.add.text(200, 50, "Settings", { fontSize: "15px", fill: "#fff" });
+    this.anims.create({
+      key: "shootMegaMagnetAnim",
+      frameRate: 4,
+      frames: this.anims.generateFrameNumbers("magnet", {
+        start: 0,
+        end: 3,
+      }),
+      repeat: -1,
+    });
 
-    this.add.text(100, 150, "Volume", { fontSize: "15px", fill: "#fff" });
-    this.add.image(220, 155, "slider").setScale(3);
-    const handle = this.add.image(250, 155, "handle").setScale(3);
+    let volume = 100;
+    this.add.image(225, 225, "bg-box");
+    this.add.text(150, 50, "Settings", basicFontConfig);
+    this.add.text(150, 175, "How to Play", basicFontConfig);
+    this.add.text(50, 200, instructions, {
+      fontSize: "15px",
+      fill: "#fff",
+      wordWrap: { width: 380, useAdvancedWrap: true },
+    });
+    this.add.image(55, 380, "heal").setScale(2);
+    this.add.text(65, 375, "<-Heals you", basicFontConfig);
+    this.add.image(200, 380, "magnet").setScale(0.25);
+    this.add.text(220, 375, "<-???", basicFontConfig);
+
+    this.add.text(100, 100, "Volume", basicFontConfig);
+    this.add.image(220, 105, "slider").setScale(3);
+    const handle = this.add.image(250, 105, "handle").setScale(3);
     const leftButton = this.add
-      .image(170, 153, "leftclick")
+      .image(170, 103, "leftclick")
       .setScale(3)
       .setInteractive();
     const rightButton = this.add
-      .image(270, 153, "rightclick")
+      .image(270, 103, "rightclick")
       .setScale(3)
       .setInteractive();
+
+    const closeButton = this.add.image(410, 40, "close").setInteractive();
 
     leftButton.on("pointerup", () => {
       if (volume >= 10) {
@@ -37,6 +69,10 @@ export default class Settings extends Phaser.Scene {
         volume += 10;
         handle.x += 6;
       }
+    });
+
+    closeButton.on("pointerup", () => {
+      this.scene.start("welcome", { sfxVolume: volume });
     });
   }
 }
