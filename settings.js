dea33b2048fe75c0b970f39e12fc10980c4b1500
Rachel Kaufman <16939591+rkaufman13@users.noreply.github.com>
2023-11-13 10:21:01 -0500

    move slider

--- settings.js
+++ settings.js
@@ -11,18 +11,32 @@ export default class Settings extends Phaser.Scene {
   }
 
   create() {
+    let volume = 100;
     this.add.text(200, 50, "Settings", { fontSize: "15px", fill: "#fff" });
 
     this.add.text(100, 150, "Volume", { fontSize: "15px", fill: "#fff" });
     this.add.image(220, 155, "slider").setScale(3);
-    this.add.image(250, 155, "handle").setScale(3);
+    const handle = this.add.image(250, 155, "handle").setScale(3);
     const leftButton = this.add
-      .image(170, 155, "leftclick")
+      .image(170, 153, "leftclick")
       .setScale(3)
       .setInteractive();
     const rightButton = this.add
-      .image(265, 155, "rightclick")
+      .image(270, 153, "rightclick")
       .setScale(3)
       .setInteractive();
+
+    leftButton.on("pointerup", () => {
+      if (volume >= 10) {
+        volume -= 10;
+        handle.x -= 6;
+      }
+    });
+    rightButton.on("pointerup", () => {
+      if (volume <= 90) {
+        volume += 10;
+        handle.x += 6;
+      }
+    });
   }
 }
